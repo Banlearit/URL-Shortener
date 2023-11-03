88e76cbc948fbab2fcbe7292158a75b1f9f6157a
Banlearit <banlearit.siri@gmail.com>
2023-11-04 00:28:21 +0700

    refactor : Change Endpoint / to /history

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const urlController = require('../controllers/urlController');
 
-router.get('/',urlController.getHistory);
+router.get('/history',urlController.getHistory);
 router.post('/shorten', urlController.createShortUrl);
 router.get('/:shortUrl', urlController.getUrl);
 // router.get('/qrcode/:shortUrl', urlController.generateQRCode);
