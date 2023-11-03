5a7fbe0241a3fb0ef710e683c786abd9cef251da
Banlearit <banlearit.siri@gmail.com>
2023-11-03 18:56:08 +0700

    Add : QR code generation to URL shortener

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -5,26 +5,31 @@ const PORT = process.env.PORT || 5000;
 const createShortUrl = async (req, res) => {
     try {
         const { url } = req.body;
-        const short = shortId.generate();
-        const PORT = process.env.PORT || 3000;
-        // Check if the request is secure (HTTPS) or if the 'X-Forwarded-Proto' header is set to 'https'
+
         const protocol = req.secure || req.get('X-Forwarded-Proto') === 'https' ? 'https' : 'http';
-        // Get the host from the request headers
         const host = req.get('Host');
         const baseUrl = `${protocol}://${host}`;
-        const newShortUrl = `${baseUrl}/${short}`;
 
-        // Create a short URL record
-        const shortUrlRecord = await ShortUrl.create({ full: url, short });
-        // Generate QR Code
-        const qrCode = await QRCode.toDataURL(shortUrlRecord.full);
+        const existingShortUrl = await ShortUrl.findOne({ full: url });
 
-        res.json({ shortUrl: newShortUrl, qrCode });
+        if (existingShortUrl) {
+            return respondWithShortUrl(res, existingShortUrl, baseUrl);
+        }
+
+        const short = shortId.generate();
+        const newShortUrlRecord = await ShortUrl.create({ full: url, short });
+        return respondWithShortUrl(res, newShortUrlRecord, baseUrl);
+
     } catch (error) {
         res.status(500).json({ error: 'Error in URL shortening or QR Code generation.', message: error.message });
     }
 };
 
+const respondWithShortUrl = async (res, shortUrlRecord, baseUrl) => {
+    const shortUrl = `${baseUrl}/${shortUrlRecord.short}`;
+    const qrCode = await QRCode.toDataURL(shortUrlRecord.full);
+    res.json({ shortUrl, qrCode });
+};
 
 const getUrl = async (req, res) => {
     const { shortUrl } = req.params;
