0737beda9a2cf32cd90cd6b50683d5f501e127a9
Banlearit <banlearit.siri@gmail.com>
2023-11-03 18:58:32 +0700

    Add : Rm Import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Shortener from "@/views/Shortener.vue";
 import ShortUrlDisplay from "@/components/ShortUrlDisplay.vue";
 
